befeb02b603c7a913c50f9cb83860fd786690f8f
WeariTraveller <2952706476@qq.com>
2025-08-07 14:35:26 +0800

    Complete sub-command `preset`
    which means src/lib/preset.ts is finished

--- src/lib/preset.ts
+++ src/lib/preset.ts
@@ -1,1 +1,22 @@
-export async function copyPreset(presetDirs: string[], destDir: string) {}
+import path from "node:path";
+import fs from "fs-extra";
+
+const assetsDir = path.resolve(__dirname, "../../assets");
+export async function copyPreset(presets: string[], destDir: string) {
+  for (const preset of presets) {
+    const src = path.resolve(assetsDir, preset);
+    const target = path.resolve(destDir, preset);
+
+    if (!(await fs.exists(src))) {
+      console.warn(`${preset} is unknown! Skip`);
+      continue;
+    }
+    if (await fs.exists(target)) {
+      console.log(`${preset} has existed in destination directory! Skip`);
+      continue;
+    }
+
+    await fs.copy(src, target, { overwrite: false, errorOnExist: true });
+    console.log(`${preset} has been generated!`);
+  }
+}
