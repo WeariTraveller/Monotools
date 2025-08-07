392d2e32e3d79ffe29f9d0e1742312a2b58f85e0
WeariTraveller <2952706476@qq.com>
2025-08-07 13:36:27 +0800

    Complete processing logic of sub-cmd `updateTsRef`

--- src/commands/updateTsRef.ts
+++ src/commands/updateTsRef.ts
@@ -1,1 +1,12 @@
-async function updateRef() {}
+import type { CommandModule } from "yargs";
+
+const command: CommandModule = {
+  command: "updateTsRef",
+  describe: "Update (and fix) the project references in each tsconfig.json",
+  async handler() {
+    const { updateTsRef } = await import("../lib/updateTsRef.js");
+    updateTsRef();
+  },
+};
+
+export default command;
