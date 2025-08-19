59ba596bf2289e9f4edf7cb745e256958ba1f421
WeariTraveller <2952706476@qq.com>
2025-08-19 13:57:25 +0800

    Update unit test for service/informativeCopy.ts

--- src/service/informativeCopy.test.ts
+++ src/service/informativeCopy.test.ts
@@ -12,35 +12,34 @@ jest.mock("fs-extra", () => ({
 }));
 const fse = jest.mocked(require("fs-extra"));
 
-describe("src/lib/preset.ts' pub fn copy", () => {
-  const files = ["There're", "3", "files"];
+describe("src/service/informativeCopy.ts' pub fn copy", () => {
   const log = jest.spyOn(console, "log").mockImplementation();
   const warn = jest.spyOn(console, "warn").mockImplementation();
 
   beforeEach(() => jest.clearAllMocks());
 
   it("usually works well", async () => {
-    await copy(files, ".");
+    await copy("file", "");
     expect(log.mock.calls.flat()).toEqual(
       expect.arrayOf(expect.stringMatching(/^√/)),
     );
     expect(warn).toHaveBeenCalledTimes(0);
-    expect(fse.copy).toHaveBeenCalledTimes(3);
+    expect(fse.copy).toHaveBeenCalledTimes(1);
   });
 
-  it("refuses to overwrite existed files", async () => {
+  it("inform right log to refuse to overwrite", async () => {
     fse.exists.mockResolvedValue(true);
-    await copy(["existed"], ".");
+    await copy("", "existed", { overwrite: false });
     expect(log.mock.calls[0][0]).toMatch(/^existed already existed/);
     expect(warn).toHaveBeenCalledTimes(0);
     expect(fse.copy).toHaveBeenCalledTimes(0);
   });
 
-  it("refuses to copy unknown presets", async () => {
+  it("refuses to copy unknown source", async () => {
     fse.exists.mockResolvedValue(false);
-    await copy(["undefined"], ".");
+    await copy("undefined", "");
     expect(log).toHaveBeenCalledTimes(0);
-    expect(warn.mock.calls[0][0]).toMatch(/^undefined is unknown/);
+    expect(warn.mock.calls[0][0]).toMatch(/^⚠️ undefined is unknown/);
     expect(fse.copy).toHaveBeenCalledTimes(0);
   });
 });
