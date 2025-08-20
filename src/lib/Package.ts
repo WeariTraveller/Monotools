ec918430f9ebcb430ae9ec289ea0ba7400406a9c
WeariTraveller <2952706476@qq.com>
2025-08-20 14:40:06 +0800

    Remove cls Package from Workspace.ts to Package.ts
    And basically complete it

--- src/lib/Package.ts
+++ src/lib/Package.ts
@@ -1,12 +1,14 @@
 import { join } from "node:path";
-import { readJSONSync } from "fs-extra";
+import { readJSONSync, writeJSONSync } from "fs-extra";
+import { readonlyHandler } from "./usefulProxies.js";
 
 export class Package {
   readonly path: string;
   /** Transparent and non-extensible packageing of package.json */
   readonly manifest: Object;
   /** A proxy whose properties you can get and set just like you are
-   * operating the package.json */
+   * operating the package.json.
+   * Don't use Object.defineProperty on it, or you'll get SyntaxError */
   readonly manifestProxy: Object;
 
   constructor(path: string) {
@@ -19,8 +21,18 @@ export class Package {
         throw new TypeError(
           "package.json is invalid as not parsed into an object",
         );
-      const readonlyProxy = new Proxy(manifest, {});
-      const operatableProxy = new Proxy(manifest, {});
+      const readonlyProxy = new Proxy(manifest, readonlyHandler);
+      const operatableProxy = new Proxy(manifest, {
+        set(obj, name, value, receiver) {
+          if (receiver) throw new TypeError("No setter so no receiver");
+          obj[name] = value;
+          writeJSONSync(join(path, "package.json"), obj);
+          return true;
+        },
+        defineProperty() {
+          throw new SyntaxError("Don't use defineProperty");
+        },
+      });
       return [readonlyProxy, operatableProxy];
     })();
   }
