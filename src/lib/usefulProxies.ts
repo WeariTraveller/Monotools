96ae121fde2f70c0623ce4070dcbf942d397aff5
WeariTraveller <2952706476@qq.com>
2025-08-20 14:25:12 +0800

    Create lib/usefulProxies.ts
    It exports only `readonlyHandler: ProxyHandler<Object>` now

---
+++ src/lib/usefulProxies.ts
@@ -0,0 +1,24 @@
+function noExtending(): boolean {
+  throw new TypeError("Readonly!");
+}
+
+export const readonlyHandler: ProxyHandler<Object> = {
+  set: noExtending,
+  defineProperty: noExtending,
+  deleteProperty: noExtending,
+  setPrototypeOf: noExtending,
+  isExtensible() {
+    return false;
+  },
+  getOwnPropertyDescriptor(obj, name) {
+    // `in` returns true for inherited properties while it below doesn't
+    if (!Object.getOwnPropertyDescriptor(obj, name)) return undefined;
+    return {
+      //@ts-ignore: 7053
+      value: obj[name],
+      writable: false,
+      enumerable: true,
+      configurable: false,
+    };
+  },
+};
