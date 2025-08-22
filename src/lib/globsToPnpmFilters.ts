b7fec36f5ac83ca73081524681a03cb1c295fd43
WeariTraveller <2952706476@qq.com>
2025-08-22 10:06:55 +0800

    Create lib/globsToPnpmFilters.ts

---
+++ src/lib/globsToPnpmFilters.ts
@@ -0,0 +1,20 @@
+import type { WorkspaceFilter } from "@pnpm/filter-workspace-packages";
+
+export function globsToPnpmFilters(
+  globs?: string[],
+  prodGlobs?: string[],
+): WorkspaceFilter[] {
+  let filters: WorkspaceFilter[] = [];
+  let prodFilters: WorkspaceFilter[] = [];
+  if (globs)
+    filters = globs.map((glob) => ({
+      filter: glob,
+      followProdDepsOnly: false,
+    }));
+  if (prodGlobs)
+    prodFilters = prodGlobs.map((glob) => ({
+      filter: glob,
+      followProdDepsOnly: true,
+    }));
+  return filters.concat(prodFilters);
+}
