cfe75ff39e720856eb998bb1b9c16710d3c42f44
WeariTraveller <2952706476@qq.com>
2025-08-29 13:36:23 +0800

    Perfection of `doParse`'s type assign

--- src/commands/tests/util.ts
+++ src/commands/tests/util.ts
@@ -1,6 +1,17 @@
 import type * as yargs from "yargs";
 
-export function doParse<T>(parser: yargs.Argv<T>, opts: string) {
+type PrsClbk<T> = yargs.ParseCallback<T>;
+export type PrsClbkParam<T> = Parameters<PrsClbk<T>>;
+export type ParseResult<T> = {
+  err: PrsClbkParam<T>[0];
+  argv: PrsClbkParam<T>[1];
+  output: PrsClbkParam<T>[2];
+};
+
+export function doParse<T>(
+  parser: yargs.Argv<T>,
+  opts: string,
+): Promise<ParseResult<T>> {
   // TS can't choose the correct overload, so the 2nd arg of undefined is neccessary (2025 Aug.26)
   return new Promise(resolve =>
     parser.parse(opts, undefined, (err, argv, output) =>
