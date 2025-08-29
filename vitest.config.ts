c8a0aa924e997c362ee1f839aa244edf02634c86
WeariTraveller <2952706476@qq.com>
2025-08-29 11:04:26 +0800

    Improve the config of Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [
-    AutoImport({
-      imports: ["vitest"],
-      dts: true,
-    }),
-  ],
   test: {
+    globals: true,
     mockReset: true,
   },
 });
